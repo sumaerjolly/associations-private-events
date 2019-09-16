eeaa8bc9602133264069b6a2fde4e827d0512c1f
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-16 16:15:53 +0530

    Added events controller tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.create(name: "jolly")
+    @event = Event.create(description: "Testing", date: "2019-09-26 00:00:00", creator: @user )
+  end
+
+  test "should get events index" do 
+    get events_path
+    assert_response :success
+  end
+
   test "should get new" do
-    get events_new_url
+    sign_in_as(@user)
+    get new_event_path
     assert_response :success
   end
 
   test "should get show" do
-    get events_show_url
+    sign_in_as(@user)
+    get event_path(@event)
     assert_response :success
   end
 
-  test "should get index" do
-    get events_index_url
-    assert_response :success
-  end
+  test "should redirect create when user not logged in" do
+    assert_no_difference 'Event.count' do
+        post events_path, params: { event: { description: "Testing", date: "2019-09-26 00:00:00", creator: @user  } }
+    end
+    assert_redirected_to login_path
+end
 
 end
