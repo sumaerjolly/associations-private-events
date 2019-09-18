4ceebb4896caac2dbb9f8715e9ffaab4266d4373
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-18 15:46:25 +0530

    refactored into users helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
-
-  def require_user
-    return if current_user
-
-    flash[:danger] = 'You must be logged in to perform this action'
-    redirect_to login_path
-  end
+  include UsersHelper
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def require_user
+    return if current_user
+
+    flash[:danger] = 'You must be logged in to perform this action'
+    redirect_to login_path
+  end
 end
